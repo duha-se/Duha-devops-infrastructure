92859c9d290d1dfaf8b19e25b83ea950455500f7
Duha <duha@shah2range.com>
2025-12-17 22:44:13 +0200

    Some fixes

--- backend.tf
+++ backend.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
     }
   }
 
   backend "s3" {
     bucket = "duha-terraform-state"
-    key    = "infrastructure.tfstate "
+    key    = "infrastructure.tfstate"
     region = "us-east-1"
   }
 }
