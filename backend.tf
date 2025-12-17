b1fd184108bfb769f992d618727bb21a53c6b8c5
Duha <duha@shah2range.com>
2025-12-17 20:57:51 +0200

    fix the backend.tf file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,14 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
-    bucket = "duha-terraform-state"
-    key    = "infrastructure.tfstate"
-    region = "us-east-1"
+    bucket = "my-terraform-state-bucket"
+    key    = "eks/terraform.tfstate"
+    region = "us-west-2"
   }
 }
